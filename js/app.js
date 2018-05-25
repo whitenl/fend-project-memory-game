ffd722ab26798e6f433ba04575da99c80f4aef70
Nic White <nicole.l.white@gmail.com>
2018-05-24 21:51:25 -0700

    Cards flip when clicked

--- js/app.js
+++ js/app.js
@@ -36,3 +36,15 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+//event listener to show card once it is clicked
+const cards = document.querySelectorAll('.card');
+const showCards = [];
+
+cards.forEach(function(card) {
+	card.addEventListener('click', function(e) {
+		showCards.push(card);
+		card.classList.add('open','show');
+	});
+});
+
